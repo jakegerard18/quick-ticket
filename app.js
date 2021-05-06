de9b7abe90f980f7dba46ae93e3130ed403770b1
Jake <jakegerard18@gmail.com>
2021-05-05 22:56:40 -0500

    Added getTickets() and printTickets() functions.

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-//Ticketing vars
-let ticketNum = 0;
-
-//Storage
+//Init storage, get tickets from storage, and set ticket number
 let ls = window.localStorage;
+let ticketNum = ls.length;
+let tickets = getTickets();
 
 // UI vars
 let uiNewTicketBtn = document.getElementById('new-ticket-btn');
@@ -12,36 +11,44 @@ let uiNewTicketModal = document.getElementById('new-ticket-modal');
 let uiTicketTable = document.getElementById('ticket-table');
 
 
+//Print tickets on page load
+document.addEventListener("DOMContentLoaded", () => {
+    printTickets(tickets);
+});
+
+//Event to trigger new ticket modal
 uiNewTicketBtn.addEventListener('click', () => {
     uiNewTicketModal.style.display = 'block'
 })
 
+//Event to add a new ticket
 uiModalSubmitBtn.addEventListener('click', () => {
-    addNewTicket();
+    addTicket();
     uiNewTicketModal.style.display = 'none';
     clearTicketModal();
-
     });
 
+
+//Event to cancel adding a ticket
 uiModalCancelBtn.addEventListener('click', () => {
     uiNewTicketModal.style.display = 'none';
     clearTicketModal();
 })
 
-function addNewTicket() {
+//Function to add a ticket to local storage and append it to the html table
+function addTicket() {
     ticketNum++;
     let name = document.getElementById('issue-name').value;
     let customer = document.getElementById('issue-customer').value;
     let description = document.getElementById('issue-desc').value;
-
     let ticketObj = {
         ticketNum: ticketNum,
         name: name,
         customer: customer,
         description: description
     }
-
-
+ 
+    tickets.push(ticketObj);
     ls.setItem(ticketNum, JSON.stringify(ticketObj));
     uiTicketTable.innerHTML += `            
     <tr class="ticket-row">
@@ -54,7 +61,34 @@ function addNewTicket() {
         <td class="td-btn"><a href="#" class="cancel-task"><i class="far fa-times-circle"></i></a></td>
     </tr>
     `
+}
 
+//Function to load tickets from local storage
+function getTickets() {
+    let tix = [];
+    for(var i = 1; i <= ls.length; i++)
+    {
+       tix[i-1] = JSON.parse(ls.getItem(`${i}`));
+    }
+
+    return tix;
+}
+
+//Function to print tickets in ticket array
+function printTickets(tickets) {
+    tickets.forEach(ticket => {
+        uiTicketTable.innerHTML += `            
+        <tr class="ticket-row">
+            <td class="td-num">${ticket.ticketNum}</td>
+            <td class="td-name">${ticket.name}</td>
+            <td class="td-customer">${ticket.customer}</td>
+            <td class="td-description">${ticket.description}</td>
+            <td class="td-btn"><a href="#" class="view-task"><i class="far fa-eye"></i></a></td>
+            <td class="td-btn"><a href="#" class="complete-task"><i class="fas fa-check"></i></a></td>
+            <td class="td-btn"><a href="#" class="cancel-task"><i class="far fa-times-circle"></i></a></td>
+        </tr>
+       `
+    })
 }
 
 function clearTicketModal() {
